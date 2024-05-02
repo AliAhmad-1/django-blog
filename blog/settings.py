b459019f59b7f69bf5ac70c311ec08f4723a30ee
Ali Ahmad <ali.h.ahmad0dj0@gmail.com>
2024-05-03 02:17:03 +0300

    upload project on railway

--- blog/settings.py
+++ blog/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY =str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
-
+ALLOWED_HOSTS = ['127.0.0.1','blog-dj.up.railway.app']
+CSRF_TRUSTED_ORIGINS=['https://blog-dj.up.railway.app']
 
 # Application definition
 SITE_ID = 1
