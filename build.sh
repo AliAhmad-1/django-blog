109d57a7dcbc6c9086619b78ba322161232e314a
Ali Ahmad <ali.h.ahmad0dj0@gmail.com>
2024-05-02 22:41:24 +0300

    second10 commit

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
+
+
 #!/bin/bash
-
-# build the project
-echo "Building the project"
+# Build the project
+echo "Building the project..."
 python3.9 -m pip install --upgrade pip
-pip3 --version
-echo $PATH
-pip3 install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
-echo "Make Migrations"
+echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-echo "collect static..."
+echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
