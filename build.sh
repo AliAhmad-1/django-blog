1119b6c3e6ab5f8fca98e94d7156592283a5dba2
Ali Ahmad <ali.h.ahmad0dj0@gmail.com>
2024-05-02 21:58:46 +0300

    second4 commit

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 
 # build the project
 echo "Building the project"
-pip install -r requirements.txt
+python -m pip install --upgrade pip
 
+pip3 install awscli
+pip3 install -r requirements.txt
+pip3 --version
 echo "Make Migrations"
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
