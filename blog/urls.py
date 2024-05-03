333273017fb5c4a4e22db2227d649b7ae2475536
Ali Ahmad <ali.h.ahmad0dj0@gmail.com>
2024-05-03 15:45:21 +0300

    update4

--- blog/urls.py
+++ blog/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('oauth/', include('social_django.urls', namespace='social')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
